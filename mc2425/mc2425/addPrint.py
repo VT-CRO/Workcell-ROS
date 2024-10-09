60c4a6203124103a7b533e2e56a56c3ce4af93bb
Andrew Viola <aviola@vt.edu>
2024-10-09 21:34:23 +0000

    Switched to custom message"

--- mc2425/mc2425/addPrint.py
+++ mc2425/mc2425/addPrint.py
@@ -1,23 +1,25 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from mc2425_msgs.msg import AddPart
+
 
 class addPrint(Node):
     def __init__(self):
         super().__init__('addPrint')
         # Declare and get the parameter for height
-        self.declare_parameter('height', 0)
+        self.declare_parameter('height', 0.0)
         self.declare_parameter('printer', 0)
         self.declare_parameter('name', "")
         self.declare_parameter('author', "")
 
-        heightParam = self.get_parameter('height').get_parameter_value().integer_value
+        heightParam = self.get_parameter('height').get_parameter_value().double_value
         printerNumber = self.get_parameter('printer').get_parameter_value().integer_value
         nameParam = self.get_parameter('name').get_parameter_value().string_value
         authorParam = self.get_parameter('author').get_parameter_value().string_value
 
         # Create publisher
-        self.publisher_ = self.create_publisher(String, 'mainController', 10)
+        self.publisher_ = self.create_publisher(AddPart, 'addPart', 10)
 
         # Set up a timer for async behavior, even if we only publish once
         timer_period = 0.5
@@ -26,10 +28,13 @@ class addPrint(Node):
 
     def publish_once(self,height,number,name,author):
         # Create a customizable string message
-        msg = String()
-        msg.data = f'ADD {number} {name} {author} {height}'
+        msg = AddPart()
+        msg.print_height = float(height)
+        msg.printer_number = int(number)
+        msg.part_name = name
+        msg.author = author
         self.publisher_.publish(msg)
-        self.get_logger().info('Publishing: "%s"' % msg.data)
+        self.get_logger().info(f'Publishing: {msg.printer_number} {msg.print_height}, {msg.part_name} {msg.author}')
 
         # Stop the timer after publishing once
         self.destroy_timer(self.timer)
--- mc2425/mc2425/mainController.py
+++ mc2425/mc2425/mainController.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+from mc2425_msgs.msg import AddPart
 
 from mc2425.shelf import partsShelf
 from mc2425.gcode import gcode
@@ -11,21 +12,21 @@ HEIGHT = 1200
 class MainController(Node):
     def __init__(self):
         super().__init__('mainController')
-        self.subscription = self.create_subscription(
-            String,
-            'mainController',
-            self.listener_callback,
+        self.addSubscription = self.create_subscription(
+            AddPart,
+            'addPart',
+            self.addPart_callback,
             10)
-        self.subscription  # prevent unused variable warning
+        self.addSubscription  # prevent unused variable warning
         self.shelf = partsShelf(SLOT_NUMBER, HEIGHT)
 
-    def addPart(self,data):
+    def addPart(self,name,author,height):
         """
         Adds a part to the shelf and tells the printer to go to a specific slot
         :param data: List of data from a TCP socket request
         :return: String to send back to client
         """
-        part = gcode(data[1], data[2], float(data[3]))
+        part = gcode(name, author, float(height))
         confirm, slot = self.shelf.addPart(part)
         if confirm:
             return f"Part added successfully in slot {slot}"
@@ -36,14 +37,13 @@ class MainController(Node):
         if function == "ADD":
             self.get_logger().info(self.addPart(parameters))
 
-
-
-    def listener_callback(self, msg):
-        self.get_logger().info('I heard: "%s"' % msg.data)
-        data = msg.data.split()
-        function = data[0]
-        data.pop(0)
-        self.utilizeData(function,data)
+    def addPart_callback(self, msg):
+        self.get_logger().info(f'Received: {msg.printer_number} {msg.print_height} {msg.part_name} {msg.author}')
+        self.get_logger().info(self.addPart(msg.part_name,msg.author,msg.print_height))
+        # data = msg.data.split()
+        # function = data[0]
+        # data.pop(0)
+        # elf.utilizeData(function,data)
 
 
 
