133f5242af761e087f869e094d1f3d6fcee42245
Andrew Viola <aviola@vt.edu>
2024-12-30 15:34:07 -0500

    Made klipper2ros a bit more robust, but annoying to use

--- klipper2ros.py
+++ klipper2ros.py
@@ -1,10 +1,7 @@
 import socket
 import argparse
 
-socket_path = "/tmp/printer_socket"
-
-
-def main(message):
+def main(message,socket_path):
     try:
         with socket.socket(socket.AF_UNIX, socket.SOCK_DGRAM) as client_socket:
             client_socket.sendto(message.encode("utf-8"), socket_path)
@@ -14,11 +11,11 @@ def main(message):
 
 
 if __name__ == "__main__":
-    parser = argparse.ArgumentParser(description="Send a message to the printer")
-    parser.add_argument("message", type=str, nargs="+", help="Message to send")
+    parser = argparse.ArgumentParser(description="Send a message to a socket.")
+    parser.add_argument("-s", "--socketLoc", required=True, default="/tmp/printer_socket", help="Path to the socket")
+    parser.add_argument("-m", "--message", required=True, nargs="+", help="Message to send")
     args = parser.parse_args()
     message = args.message
     if type(message) == list:
         message = " ".join(message)
-    print(message)
-    main(message)
+    main(message,args.socketLoc)
